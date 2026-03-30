2dbb0307d7556a322b579930301f52cc300b117d
うゆてこちん <uyutako2@gmail.com>
2026-03-30 22:47:06 +0900

    Add initial ranking data with scores

---
+++ ranking.js
@@ -0,0 +1,9 @@
+const scores = [
+  { name: 'うゆり＆てこ', score: 3144 },
+  { name: 'にこに昆布', score: 92 },
+  { name: 'Y.I（多分本物）', score: 1517 },
+  { name: 'ブルーアーカイブ3世', score: 335 },
+  { name: '魔王たびま', score: 1471 },
+  { name: 'Mr.サハカズ', score: 723 },
+  { name: 'もち吉', score: 2413 }
+]
