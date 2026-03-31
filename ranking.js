2ec6bf7073a48fbf95131cb8c7df0d52a3ea047f
うゆてこちん <uyutako2@gmail.com>
2026-03-31 12:33:11 +0900

    Update score for 'もち吉' in ranking

--- ranking.js
+++ ranking.js
@@ -5,7 +5,7 @@ window.ranking = [
   { name: 'ブルーアーカイブ3世', score: 335 },
   { name: '魔王たびま', score: 1471 },
   { name: 'Mr.サハカズ', score: 723 },
-  { name: 'もち吉', score: 2413 }
+  { name: 'もち吉', score: 3009 }
 ]
 
 window.ranking.sort((a, b) => b.score - a.score);
