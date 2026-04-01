3a038f2bb45fad24f494315b4b9dccdefae301a7
うゆてこちん <uyutako2@gmail.com>
2026-04-01 12:34:56 +0900

    Add new entry to ranking and maintain existing entry

--- ranking.js
+++ ranking.js
@@ -5,7 +5,8 @@ window.ranking = [
   { name: 'ブルーアーカイブ3世', score: 335 },
   { name: '魔王たびま', score: 1471 },
   { name: 'Mr.サハカズ', score: 723 },
-  { name: 'もち吉', score: 3592 }
+  { name: 'もち吉', score: 3592 },
+  { name: 'カナタ&千姫', score: 1613 }
 ]
 
 window.ranking.sort((a, b) => b.score - a.score);
