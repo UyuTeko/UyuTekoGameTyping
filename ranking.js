c798c71fef49ef2ec0c6d1925a52707b84e657f1
うゆてこちん <uyutako2@gmail.com>
2026-09-24 18:22:22 +0900

    Add new entry for 'ルイ' to ranking list

--- ranking.js
+++ ranking.js
@@ -10,7 +10,8 @@ window.ranking = [
   { name: '竜真', score: 1520 },
   { name: '飛香', score: 2601 },
   { name: 'robotomizuki', score: 1164 },
-  { name: 'Axeuo🔥『ハック』', score: 1272 }
+  { name: 'Axeuo🔥『ハック』', score: 1272 },
+  { name: 'ルイ', score: 2954 }
 ]
 
 window.ranking.sort((a, b) => b.score - a.score);
